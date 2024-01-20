a2337f4bf7391161d8e81e48bbb516bcf7569886
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-20 16:55:27 +0900

    여러줄 렌더링

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 function App() {
-  return <h1>안녕하세요.</h1>;
+  return (
+    <>
+      <h1>안녕하세요.</h1>
+      <br />
+      <p className="description">리액트 기초 교육입니다.</p>
+    </>
+  );
 }
 export default App;
