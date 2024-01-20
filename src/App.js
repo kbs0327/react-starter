055821ddc3d191dd63fe8aa85098d37ea666c98f
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-20 17:00:28 +0900

    조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Description from './components/Description';
+import Hello from './components/Hello';
 function App() {
   return (
     <>
-      <h1>안녕하세요.</h1>
-      <br />
-      <p className="description">리액트 기초 교육입니다.</p>
+      <Hello isChild={false}/>
+      <Description isOpen={true}/>
     </>
   );
 }
---
+++ src/components/Description.js
@@ -0,0 +1,4 @@
+function Description({isOpen}) {
+  return isOpen && <p>열렸습니다.</p>;
+}
+export default Description;
