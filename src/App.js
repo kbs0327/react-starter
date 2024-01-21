786594b76e07eedb149c56002d31322625dd65c4
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-21 13:38:32 +0900

    ex7 - 컴포넌트 쪼개기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 function App() {
+  // FIXME components/TodoApp.js 로 분리
   const todos = [
     {
       value: "교육",
@@ -16,32 +17,46 @@ function App() {
       id: 3,
     },
   ];
+
+  // FIXME components/Footer.js 로 분리
   const activeTodoCount = todos.filter((todo) => !todo.checked).length;
   return (
     <>
+      {/* FIXME components/TodoApp.js 로 분리 */}
       <section className="todoapp">
+        {/* FIXME components/Header.js 로 분리 */}
         <header>
           <h1>todos</h1>
         </header>
+        {/* FIXME components/Header.js 로 분리 */}
         {todos.length > 0 && (
           <>
+            {/* FIXME components/Main.js 로 분리 */}
             <main className="main">
+              {/* FIXME components/TodoList.js 로 분리 */}
               <ul className="todo-list">
                 {todos.map((todo) => (
+                  // {/* FIXME components/TodoItem.js 로 분리 */}
                   <li key={todo.id} className={todo.checked ? "completed" : ""}>
                     <label>{todo.value}</label>
                   </li>
+                  // {/* FIXME components/TodoItem.js 로 분리 */}
                 ))}
               </ul>
+              {/* FIXME components/TodoList.js 로 분리 */}
             </main>
+            {/* FIXME components/Main.js 로 분리 */}
+            {/* FIXME components/Footer.js 로 분리 */}
             <footer className="footer">
               <span className="todo-count">
                 <strong>{activeTodoCount}</strong> items left
               </span>
             </footer>
+            {/* FIXME components/Footer.js 로 분리 */}
           </>
         )}
       </section>
+      {/* FIXME components/TodoApp.js 로 분리 */}
 
       <footer className="info">
         <p>Double-click to edit a todo</p>
