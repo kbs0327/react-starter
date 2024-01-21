e56ce8a63b2dc8c027744d13973c1c9a498af46f
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-21 11:26:23 +0900

    ex1 - jsx 예제 결과

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 function App() {
-  return <h1>todos</h1>;
+  return (
+    <>
+      <section className="todoapp">
+        <header>
+          <h1>todos</h1>
+        </header>
+      </section>
 
-  // footer 문구
-  /* <p>Double-click to edit a todo</p>
-     <p>Created by the TodoMVC Team</p>
-     <p>
-       Part of <a href="http://todomvc.com">TodoMVC</a>
-     </p> */
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>Created by the TodoMVC Team</p>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
