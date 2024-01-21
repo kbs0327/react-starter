c8fcc09ac869a2909b40f2bf28792508aff0a49f
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-21 11:43:03 +0900

    ex2 - 변수 사용 및 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 function App() {
+  const todos = [
+    {
+      value: "교육",
+      checked: false,
+      id: 1,
+    },
+  ];
+  const activeTodoCount = todos.filter((todo) => !todo.checked).length;
   return (
     <>
       <section className="todoapp">
         <header>
           <h1>todos</h1>
         </header>
+        {/* FIXME main과 footer는 todos가 있는 경우에만 보이게 수정 */}
+        <main className="main">
+          <ul className="todo-list">
+            {/* FIXME checked일 때에만 li에 completed class가 보이게 수정 */}
+            <li>
+              <label>{/* FIXME todo value 보이게 수정 */}</label>
+            </li>
+          </ul>
+        </main>
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>{/* FIXME activeTodoCount 보이게 수정 */}</strong> items left
+          </span>
+        </footer>
       </section>
 
       <footer className="info">
