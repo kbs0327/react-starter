61cabc7a8a5e3657a5db4a54b8f7278b1c224958
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-21 16:52:54 +0900

    ex11 - useRef

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import TodoInput from "./TodoInput";
 function Header() {
   return (
     <header>
+      {/* FIXME todos를 클릭하면 input이 focus 되게 수정 */}
       <h1>todos</h1>
       <TodoInput />
     </header>
