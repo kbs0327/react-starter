20673cf421477770970c1e1a3431546bdebd96fd
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-21 11:22:25 +0900

    ex1 - jsx 예제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
   return <h1>todos</h1>;
+
+  // footer 문구
+  /* <p>Double-click to edit a todo</p>
+     <p>Created by the TodoMVC Team</p>
+     <p>
+       Part of <a href="http://todomvc.com">TodoMVC</a>
+     </p> */
 }
 
 export default App;
