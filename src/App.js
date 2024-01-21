608ced7820afefc2069dd84fb42c014dad30a677
김부승/프론트엔드개발팀/ND <buseung.kim@nhndooray.com>
2024-01-21 12:31:20 +0900

    ex5 - 객체 변수 렌더링 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>todos</h1>;
-}
+import {useEffect, useMemo, useState} from 'react';
+import Title from './components/Title';
 
+function App() {
+  const [, setState] = useState({});
+
+  const style = useMemo(() => ({color: 'blue'}), []);
+
+  useEffect(() => {
+    console.log('App rerender trigger');
+    setState({});
+  }, []);
+
+  return <Title style={{color: 'blue'}}>안녕하세요.</Title>;
+}
 export default App;
